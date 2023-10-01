4488d58463d1bdd893bb5a0b53c079eb84b6feef
David Olagookun <erijesudo@gmail.com>
2023-10-01 11:57:49 +0100

    Initial Commit

---
+++ WellnessAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WellnessAI
+//
+//  Created by David OH on 01/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WellnessAI/WellnessAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WellnessAIApp.swift
+//  WellnessAI
+//
+//  Created by David OH on 01/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct WellnessAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
