a8ea2cedcf8c872b7572560f209d011e77bda079
tesddev <tesleem.amuda@gmail.com>
2023-10-02 18:32:17 +0100

    add UI for chat page

---
+++ WellnessAI/Models/Message.swift
@@ -0,0 +1,26 @@
+//
+//  Message.swift
+//  WellnessAI
+//
+//  Created by Tes on 02/10/2023.
+//
+
+import Foundation
+
+struct Message: Hashable {
+    var content: String
+    var user: User
+}
+
+struct DataSource {
+    static let firstUser = User(name: "Wellness AI", avatar: "openai_logo")
+    static var secondUser = User(name: "Duy Bui", avatar: "myAvatar", isCurrentUser: true)
+    static let messages = [
+        Message(content: "Hi, I really love your templates and I would like to buy the chat template", user: DataSource.firstUser),
+        Message(content: "Thanks, nice to hear that, can I have your email please?", user: DataSource.secondUser),
+        Message(content: "That won't be a problem", user: DataSource.firstUser),
+        Message(content: "Oh actually, I have just purchased the chat template, so please check your email, you might see my order", user: DataSource.firstUser),
+        Message(content: "Great, wait me a sec, let me check", user: DataSource.secondUser),
+        Message(content: "Sure", user: DataSource.firstUser)
+    ]
+}
---
+++ WellnessAI/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  WellnessAI
+//
+//  Created by Tes on 02/10/2023.
+//
+
+import Foundation
+
+struct User: Hashable {
+    var name: String
+    var avatar: String
+    var isCurrentUser: Bool = false
+}
