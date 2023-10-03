b23d531fac7a0313d3ca16385dae31dd85ae1dda
tesddev <tesleem.amuda@gmail.com>
2023-10-03 22:32:59 +0100

    remove dummy data

--- WellnessAI/Network/LoginViewModel.swift
+++ WellnessAI/Network/LoginViewModel.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import Foundation
 
 class LoginVM: ObservableObject{
-    @Published var person = LoginPayload(email: "test@mail.com", password: "password")
+    @Published var person = LoginPayload()
     @Published private(set) var userInfo : LoginResponse?
     @Published private(set) var state : SubmissionState?
     @Published private(set) var error: NetworkingManager.NetworkingError?
